e24932c92844f7721ab075baedb5b9685dfd8d81
TuNV <tunv@unitech.vn>
2024-12-19 12:06:48 +0900

    chat message

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('chat');
+})->middleware('auth');
+
+Route::get('/getUserLogin', function () {
+    return Auth::user();
+})->middleware('auth');
+
+Route::get('/messages', function () {
+    return App\Models\Message::with('user')->get();
+})->middleware('auth');
+
+Route::post('/messages', function () {
+    $user = Auth::user();
+
+    $message = new App\Models\Message();
+    $message->message = request()->get('message', '');
+    $message->user_id = $user->id;
+    $message->save();
+
+    return ['message' => $message->load('user')];
+})->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
