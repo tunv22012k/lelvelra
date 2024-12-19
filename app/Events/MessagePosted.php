c6e48bf59e19f848e517d5fe795917f7aeb8059a
TuNV <tunv@unitech.vn>
2024-12-19 13:37:16 +0900

    file config chat realtime

---
+++ app/Events/MessagePosted.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessagePosted implements ShouldBroadcast
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public $message;
+    public $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Message $message, User $user)
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return ['chatroom'];
+        // hoặc: return new Channel('chatroom');
+    }
+}
+
